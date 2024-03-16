bfed3092c3c5fa4eb2e0a0f902e903ea04e1aa07
Sneha7112 <143403183+Sneha7112@users.noreply.github.com>
2024-03-16 11:43:29 +0530

    commit

--- qrCode_barCode.py
+++ qrCode_barCode.py
@@ -1,21 +1,46 @@
 import cv2
-import numpy as np
 from pyzbar.pyzbar import decode
 
-#for image
-#img = cv2.imread('qrcodeimage1.jpg')
-# code = decode(img)   #prints all the information like rect , data, type 
-# print(code)
 
-#for webcam
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-while True:
-     ret, img = cap.read()
-     for barcode in decode(img):
-        print(barcode.data)     #prints only the data
-        myData = barcode.data.decode('utf-8')
-        print(myData)
-     cv2.imshow('result',img)
-     cv2.waitKey(0)
+def scan_codes(img):
+    barcodes = decode(img)
+    for barcode in barcodes:
+        myData = barcode.data.decode('utf-8') #data Extractiom
+        # Drawing box 
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Displaying type and data
+        cv2.putText(img, str(myData), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        print(f"Type: {barcode.type}, Data: {myData}")
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)  # frame width
+    cap.set(4, 480)  # frame height
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame from webcam.")
+            break
+
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Scanning 
+        scan_codes(gray)
+
+        # frame display
+        cv2.imshow('Barcode and QR Code Scanner', frame)
+
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
