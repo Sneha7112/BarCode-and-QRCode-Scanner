8bdfc879ff32f673687c0a019f10e5e72820d8d9
Sneha7112 <143403183+Sneha7112@users.noreply.github.com>
2024-03-16 10:27:55 +0530

    capturing image
    it will capture qrcode or barcode from web cam and gives data of that in the form of utf-8.

--- qrCode_barCode.py
+++ qrCode_barCode.py
@@ -1,3 +1,21 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+
+#for image
+#img = cv2.imread('qrcodeimage1.jpg')
+# code = decode(img)   #prints all the information like rect , data, type 
+# print(code)
+
+#for webcam
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+while True:
+     ret, img = cap.read()
+     for barcode in decode(img):
+        print(barcode.data)     #prints only the data
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+     cv2.imshow('result',img)
+     cv2.waitKey(0)
